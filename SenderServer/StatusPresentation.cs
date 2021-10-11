2e7c38b089739145a5f12a41785e41c3e2003c20
RudnevI <irudnev@protonmail.com>
2021-10-11 21:46:57 +0600

    Добавьте файлы проекта.

---
+++ SenderServer/StatusPresentation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenderServer
+{
+    class StatusPresentation
+    {
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+
+        public string Status { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0:HH:mm:ss} - {1}", TimeStamp, Status);
+        }
+    }
+}
